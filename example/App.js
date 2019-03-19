4554e8271c9c1d2ba0dde40f6bcee4c3c66a4bca
Hien Phung <hien.phung@nfq.asia>
2019-03-19 16:19:52 +0700

    Modify example

--- example/App.js
+++ example/App.js
@@ -1,15 +1,29 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import CallerId from "react-native-caller-id";
 
 const instructions = Platform.select({
-  ios: 'Enable the extension in Settings > Phone > Call Blocking & Identification',
+  ios: 'Enable the extension in Settings > Phone > Call Blocking & Identification\nAdd more callers in id.json file',
   android:
-    'Use the number which you provided in the callers list to call.\nYou should see an overlay.',
+    'Use the number which you provided in the callers list to call.\nYou should see an overlay.\nAdd more callers in id.json file',
 });
+
 
 type Props = {};
 export default class App extends Component<Props> {
+  
+  addCaller() {
+    const callers = require("./id.json").callers;
+    if(callers && callers.length > 0) {
+      CallerId.setCallerList(callers);
+    }
+  }
+  
+  componentDidMount(): void {
+    this.addCaller();
+  }
+
   render() {
     return (
       <View style={styles.container}>
---
+++ ios/CallerIDExtension/CallDirectoryHandler.h
@@ -0,0 +1,15 @@
+//
+//  CallDirectoryHandler.h
+//  CallerIDExtension
+//
+//  Created by Phung Khanh Hien on 3/19/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CallKit/CallKit.h>
+
+@interface CallDirectoryHandler : CXCallDirectoryProvider
+
+@end
+
